d2cbc899420967bb80a374a4b52c783ced1e1a0f
YA <YA-androidapp@users.noreply.github.com>
2021-04-23 20:08:38 +0900

    Update the myapp

--- infra/docker/python/myapp/urls.py
+++ infra/docker/python/myapp/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name = 'myapp'
 
 urlpatterns = [
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('', views.WelcomeView.as_view(), name='welcome'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('', WelcomeView.as_view(), name='welcome'),
+
+    path('posts/', IndexView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
+    path('category/<str:category_slug>/',
+         CategoryPostView.as_view(), name='category_post'),
+    path('tag/<str:tag_slug>/', TagPostView.as_view(), name='tag_post'),
 ]
--- infra/docker/python/myapp/views.py
+++ infra/docker/python/myapp/views.py
@@ -1,9 +1,75 @@
 from django.views import generic
 
+from .models import Post, Category, Tag
+from django.db.models import Count, Q
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
+
+# Auth
 class ProfileView(generic.TemplateView):
     template_name = 'profile.html'
 
 
 class WelcomeView(generic.TemplateView):
     template_name = 'welcome.html'
+
+
+# Blog
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if not obj.is_public and not self.request.user.is_authenticated:
+            raise Http404
+        return obj
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'myapp/index.html'
+
+
+class CategoryListView(ListView):
+    queryset = Category.objects.annotate(
+        num_posts=Count('post', filter=Q(post__is_public=True)))
+
+
+class TagListView(ListView):
+    queryset = Tag.objects.annotate(num_posts=Count(
+        'post', filter=Q(post__is_public=True)))
+
+
+class CategoryPostView(ListView):
+    model = Post
+    template_name = 'myapp/category_post.html'
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        self.category = get_object_or_404(Category, slug=category_slug)
+        qs = super().get_queryset().filter(category=self.category)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
+class TagPostView(ListView):
+    model = Post
+    template_name = 'myapp/tag_post.html'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        qs = super().get_queryset().filter(tags=self.tag)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
